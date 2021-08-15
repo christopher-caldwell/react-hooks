df769aceec912bd7c6d52641469498af5d3e8cca
christopher-caldwell <well.done.514@gmail.com>
2021-08-15 12:45:02 -0600

    dns prefetching tag manager

--- src/analytics/Head.tsx
+++ src/analytics/Head.tsx
@@ -5,6 +5,7 @@ const isRunningLocally = process.env.NODE_ENV !== 'production'
 export const GtagHeadScripts: FC<{ id: string }> = ({ id }) =>
   isRunningLocally ? null : (
     <Fragment>
+      <link rel='dns-pretech' href='https://www.googletagmanager.com' />
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`}></script>
       <script
         dangerouslySetInnerHTML={{
