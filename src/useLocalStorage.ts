673d8170dd16178e3a2007f11c836065f6ce12a1
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2021-09-06 02:09:46 -0700

    re working local storage

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -2,37 +2,42 @@
  * @source https://usehooks.com/useLocalStorage/
  */
 
-import { useState, useCallback, Dispatch, SetStateAction } from 'react'
+import { useState, useCallback, Dispatch, SetStateAction, useEffect } from 'react'
 
 import { useWindow } from './useWindow'
 
 export const useLocalStorage = <T>(key: string, initialValue: T, shouldLogErrors?: boolean): UseLocalStorage<T> => {
   const { localWindow } = useWindow()
+  const isComplex = typeof initialValue !== 'string'
 
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = localWindow.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      shouldLogErrors && console.error('[useLocalStorageError]:', error)
-      return initialValue
-    }
-  })
+  const [storedValue, setStoredValue] = useState<T>(initialValue)
+
+  useEffect(() => {
+    if (!localWindow?.localStorage) return
+    const prevStoredValue = localWindow?.localStorage?.getItem(key)
+    console.log('prevStoredValue', prevStoredValue)
+    if (!prevStoredValue) return
+    setStoredValue(isComplex ? JSON.parse(prevStoredValue) : prevStoredValue)
+  }, [localWindow, key, isComplex])
 
   /** Sets given value to local storage, and syncs the state value  */
   const setValue = useCallback(
     (value: T) => {
       try {
         setStoredValue(value)
-        window.localStorage.setItem(key, JSON.stringify(value))
+        localWindow?.localStorage?.setItem(key, isComplex ? JSON.stringify(value) : (value as unknown as string))
       } catch (error) {
         shouldLogErrors && console.error('[useLocalStorageError]:', error)
       }
     },
-    [key, shouldLogErrors]
+    [key, shouldLogErrors, localWindow, isComplex]
   )
 
   return [storedValue, setValue]
 }
 
+export interface Options {
+  shouldLogErrors?: boolean
+  isComplex?: boolean
+}
 export type UseLocalStorage<T> = [T, Dispatch<SetStateAction<T>>]
--- src/useWindow.ts
+++ src/useWindow.ts
@@ -4,13 +4,13 @@ import { useState, useEffect } from 'react'
  * When this is running outside the browser, regular `window` will be undefined and cause runtime errors
  */
 export const useWindow = () => {
-  const initialWindow = typeof window !== 'undefined' ? ({} as Window) : window
+  const initialWindow = typeof window === 'undefined' ? ({} as Window) : window
   const [localWindow, setLocalWindow] = useState<Window>(initialWindow)
   useEffect(() => {
-    if (window) setLocalWindow(window)
+    if (typeof window) setLocalWindow(window)
   }, [])
 
-  const isMobile = (window && window.innerWidth <= 600) || false
+  const isMobile = (initialWindow && initialWindow.innerWidth <= 600) || false
 
   return { localWindow, isMobile }
 }
