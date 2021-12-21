ec07aac3bd7bf43160b434ba1a6c88f3b9763227
christopher-caldwell <well.done.514@gmail.com>
2021-12-20 19:04:03 -0700

    fix: Better checks for primitive values

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -8,29 +8,33 @@ import { useWindow } from './useWindow'
 
 export const useLocalStorage = <T>(key: string, initialValue: T, shouldLogErrors?: boolean): UseLocalStorage<T> => {
   const { localWindow } = useWindow()
-  const isComplex = typeof initialValue !== 'string'
+  const isPrimitive = determineIsPrimitive(initialValue)
 
   const [storedValue, setStoredValue] = useState<T>(initialValue)
 
   useEffect(() => {
     if (!localWindow?.localStorage) return
     const prevStoredValue = localWindow?.localStorage?.getItem(key)
-    console.log('prevStoredValue', prevStoredValue)
     if (!prevStoredValue) return
-    setStoredValue(isComplex ? JSON.parse(prevStoredValue) : prevStoredValue)
-  }, [localWindow, key, isComplex])
+    try {
+      setStoredValue(isPrimitive ? prevStoredValue : JSON.parse(prevStoredValue))
+    } catch (e) {
+      if (shouldLogErrors)
+        console.error('[useLocalStorageError] Failed to parse stored value. Attempted to parse:', prevStoredValue)
+    }
+  }, [localWindow, key, isPrimitive, shouldLogErrors])
 
   /** Sets given value to local storage, and syncs the state value  */
   const setValue = useCallback(
     (value: T) => {
       try {
         setStoredValue(value)
-        localWindow?.localStorage?.setItem(key, isComplex ? JSON.stringify(value) : (value as unknown as string))
+        localWindow?.localStorage?.setItem(key, isPrimitive ? (value as unknown as string) : JSON.stringify(value))
       } catch (error) {
         shouldLogErrors && console.error('[useLocalStorageError]:', error)
       }
     },
-    [key, shouldLogErrors, localWindow, isComplex]
+    [key, shouldLogErrors, localWindow, isPrimitive]
   )
 
   return [storedValue, setValue]
@@ -41,3 +45,10 @@ export interface Options {
   isComplex?: boolean
 }
 export type UseLocalStorage<T> = [T, Dispatch<SetStateAction<T>>]
+
+const determineIsPrimitive = <TData>(val: TData): boolean => {
+  if (val === null) return true
+
+  if (typeof val == 'object' || typeof val == 'function') return false
+  return true
+}
